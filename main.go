6b728088dc655cfaea44dbd8c669d2cd352498d0
Gvardmeister <alexgvardin@gmail.com>
2025-04-17 17:36:42 +0700

    Финальная часть программы

--- main.go
+++ main.go
@@ -20,58 +20,34 @@ func plusNumber(number int, MB map[string]int) {
 	}
 }
 
+func minusNumber(number int, MB map[string]int) {
+	countMap := 1
+	strKey := strconv.Itoa(number)
+	MB[strKey] = number
+
+	for countMap != 4 {
+		number--
+		strKey := strconv.Itoa(number)
+		MB[strKey] = number
+		countMap++
+	}
+}
+
 func main() {
 	var target string
 	mapBattle := make(map[string]int)
 
 	count := 0
-	countMap := 1
 	ship := []string{"@", "@", "@", "@", "@", "@", "@", "@", "@", "@"}
 
 	generator := rand.New(rand.NewSource(time.Now().UnixNano()))
 	numberGenerator := generator.Intn(10) + 1
 
-	// тестирую этот кусок
 	if numberGenerator >= 1 && numberGenerator <= 7 {
 		plusNumber(numberGenerator, mapBattle)
-
-		// strKey := strconv.Itoa(numberGenerator)
-		// mapBattle[strKey] = numberGenerator
-		//
-		// for countMap != 4 {
-		// numberGenerator++
-		// strKey := strconv.Itoa(numberGenerator)
-		// mapBattle[strKey] = numberGenerator
-		// countMap++
-		// }
 	} else {
-		strKey := strconv.Itoa(numberGenerator)
-		mapBattle[strKey] = numberGenerator
-
-		for countMap != 4 {
-			numberGenerator--
-			strKey := strconv.Itoa(numberGenerator)
-			mapBattle[strKey] = numberGenerator
-			countMap++
-		}
+		minusNumber(numberGenerator, mapBattle)
 	}
-
-	// рабочий кусок, но с багнутой генерацией
-	// for {
-	// numberGenerator := generator.Intn(10) + 1
-	// strKey := strconv.Itoa(numberGenerator)
-	//
-	// if numberGenerator, ok := mapBattle[strKey]; ok {
-	// continue
-	// } else {
-	// mapBattle[strKey] = numberGenerator
-	// countMap++
-	// }
-	//
-	// if countMap == 4 {
-	// break
-	// }
-	// }
 
 	for {
 		fmt.Println("\nВведите координату: ")
