fa519c8d63c2f30fe43fa94fd1ef54fcde61c73e
Gvardmeister <alexgvardin@gmail.com>
2025-05-24 17:13:57 +0700

    Реализовал структуры. Подготовил констукторы к структурам

--- main.go
+++ main.go
@@ -5,9 +5,10 @@ import (
 )
 
 const (
-	deck   = "X"
-	loss   = "."
-	change = "@"
+	deck      = "X"
+	loss      = "."
+	change    = "@"
+	sizeBoard = 10
 )
 
 type game struct {
@@ -31,8 +32,52 @@ type ship struct {
 }
 
 type coordinate struct {
-	x int
-	y int
+	xcoordinate int
+	ycoordinate int
+}
+
+func newGame() *game {
+	board := newBoard(sizeBoard)
+	player := newPlayer()
+
+	return &game{
+		player: *player,
+		board:  *board,
+	}
+}
+
+func newBoard(sizeBoard int) *board {
+	grid := make([][]string, sizeBoard)
+
+	for i := 0; i < sizeBoard; i++ {
+		grid[i] = make([]string, sizeBoard)
+		for j := 0; j < sizeBoard; j++ {
+			grid[i][j] = change
+		}
+	}
+
+	return &board{
+		grid:  grid,
+		ships: []ship{},
+	}
+}
+
+func newPlayer() *player {
+	return &player{}
+}
+
+func newCoordinate(x, y int) coordinate {
+	return coordinate{
+		xcoordinate: x,
+		ycoordinate: y,
+	}
+}
+
+func newShip(cells []coordinate) *ship {
+	return &ship{
+		cells: cells,
+		hit:   make([]bool, len(cells)),
+	}
 }
 
 func initGame() {
