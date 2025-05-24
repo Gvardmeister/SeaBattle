21dd48856f7721575319c9707eb8e5cf4be54421
Gvardmeister <alexgvardin@gmail.com>
2025-05-24 20:17:45 +0700

    Добавил методы для кораблей на структуре board, переношу постепенно логику из функционального стиле в ООП

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 const (
@@ -115,7 +117,103 @@ func (b *board) Render() {
 	}
 }
 
+func (b *board) generateShip(sizeShip int) {
+	generator := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	for {
+		shipX := generator.Intn(sizeBoard)
+		shipY := generator.Intn(sizeBoard)
+		positionShip := generator.Intn(2)
+
+		if b.canPlaceShip(shipX, shipY, positionShip, sizeShip) {
+			b.placeShip(shipX, shipY, positionShip, sizeShip)
+			break
+		}
+	}
+}
+
+func (b *board) canPlaceShip(x, y, orientation, sizeShip int) bool {
+	dx, dy := 0, 0
+	if orientation == 0 {
+		dy = 1
+	} else {
+		dx = 1
+	}
+
+	for i := 0; i < sizeShip; i++ {
+		nx := x + dx*i
+		ny := y + dy*i
+
+		if nx < 0 || ny < 0 || nx >= sizeBoard || ny >= sizeBoard {
+			return false
+		}
+
+		for dx2 := -1; dx2 <= 1; dx2++ {
+			for dy2 := -1; dy2 <= 1; dy2++ {
+				cx := nx + dx2
+				cy := ny + dy2
+
+				if cx >= 0 && cy >= 0 && cx < sizeBoard && cy < sizeBoard {
+					if b.grid[cx][cy] == deck {
+						return false
+					}
+				}
+			}
+		}
+	}
+
+	return true
+}
+
+func (b *board) PlaceShips() {
+	variationShip := [][]int{
+		{4, 1},
+		{3, 2},
+		{2, 3},
+		{1, 4},
+	}
+
+	for _, value := range variationShip {
+		sizeShip := value[0]
+		count := value[1]
+
+		for i := 0; i < count; i++ {
+			b.generateShip(sizeShip)
+		}
+	}
+}
+
+func (b *board) placeShip(x, y, orientation, sizeShip int) {
+	dx, dy := 0, 0
+	if orientation == 0 {
+		dy = 1
+	} else {
+		dx = 1
+	}
+
+	var cells []coordinate
+
+	for i := 0; i < sizeShip; i++ {
+		nx := x + dx*i
+		ny := y + dy*i
+		cells = append(cells, newCoordinate(nx, ny))
+	}
+
+	b.ships = append(b.ships, *newShip(cells))
+}
+
+func (g *game) MakeMove() {
+
+}
+
 func initGame() {
+	g := newGame()
+	g.board.PlaceShips()
+
+	for {
+		g.board.Render()
+		g.MakeMove()
+	}
 }
 
 func main() {
