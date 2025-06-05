b5c511baff9dc270993ad69eb44008c0ef19bb65
Gvardmeister <alexgvardin@gmail.com>
2025-06-05 18:12:33 +0700

    Реализация тестов для ship.go - успешно, покрытие 100%

--- internal/domain/ship/ship_test.go
+++ internal/domain/ship/ship_test.go
@@ -2,9 +2,31 @@ package ship
 
 import (
 	"testing"
-	//"github.com/Gvardmeister/seabattle/internal/domain/coordinate"
+
+	"github.com/Gvardmeister/seabattle/internal/domain/coordinate"
 )
 
 func TestNewShip(t *testing.T) {
+	cells := []coordinate.Coordinate{
+		coordinate.NewCoordinate(1, 2),
+		coordinate.NewCoordinate(2, 1),
+		coordinate.NewCoordinate(1, 1),
+		coordinate.NewCoordinate(-1, 1),
+		coordinate.NewCoordinate(1, -1),
+	}
 
+	ship := NewShip(cells)
+
+	if len(cells) != len(ship.Cells) {
+		t.Errorf("Ожидалась длина Cells = %d, получено %d", len(cells), len(ship.Cells))
+	}
+	if len(cells) != len(ship.Hit) {
+		t.Errorf("Ожидалась длина Hit = %d, получено %d", len(cells), len(ship.Hit))
+	}
+
+	for i, hit := range ship.Hit {
+		if hit {
+			t.Errorf("Hit[%d] должен быть false по умолчанию", i)
+		}
+	}
 }
