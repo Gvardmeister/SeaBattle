92d957dd583fe9a4799dac2beec772f2d7eaf580
Gvardmeister <alexgvardin@gmail.com>
2025-06-06 18:38:51 +0700

    Реализована мок-структура

--- internal/test/mocks/mockinputreader/inputReaderMock.go
+++ internal/test/mocks/mockinputreader/inputReaderMock.go
@@ -1,1 +1,23 @@
 package mockinputreader
+
+import "errors"
+
+type MockInputReader struct {
+	inputs []string
+	index  int
+}
+
+func NewMockInput(inputs []string) *MockInputReader {
+	return &MockInputReader{
+		inputs: inputs,
+	}
+}
+
+func (m *MockInputReader) ReadLine() (string, error) {
+	if m.index >= len(m.inputs) {
+		return "", errors.New("no more input")
+	}
+	input := m.inputs[m.index]
+	m.index++
+	return input, nil
+}
